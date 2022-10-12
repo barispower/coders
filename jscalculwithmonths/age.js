2560f22833ec81995ef80914d781ab18b952d68b
barisgit <baris.durucan@powercoders.org>
2022-10-12 22:37:56 +0200

    jscalcul withmonths

---
+++ jscalculwithmonths/age.js
@@ -0,0 +1,15 @@
+var birthYear = 1984;
+var futureYear  = 2027;
+var age  = futureYear - birthYear;
+console.log('I will be either ' + age + ' or ' + (age - 1) + ' in ' + futureYear);
+
+
+
+/* OTHER VERSION
+const birthYear = 1984;
+let futureYear = 2067;
+let ageBeforeBirthday = futureYear - birthYear;
+let ageAfterBirthday = ageBeforeBirthday - 1;
+document.write('I will be either ' + ageBeforeBirthday + ' or ' + ageAfterBirthday + ' in ' + futureYear);
+
+*/
