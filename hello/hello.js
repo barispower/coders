a23ad929a60430f5e39221176046b642997a95ad
barisgit <baris.durucan@powercoders.org>
2022-10-17 20:35:02 +0200

    hello in different languages

---
+++ hello/hello.js
@@ -0,0 +1,23 @@
+function helloWorld(lang) {
+    if (lang == "fr") {
+      return "Bonjour, monde";
+    } else if (lang == "es") {
+      return "Hola, Mundo";
+    } else {
+      return "Hello, World";
+    }
+  }
+  function helloWorld(lang) {
+    if (lang == "fr") {
+      return "Bonjour, monde";
+    } else if (lang == "es") {
+      return "Hola, Mundo";
+    } else {
+      return "Hello, World";
+    }
+  }
+  alert(helloWorld("en"));
+  alert(helloWorld("fr"));
+  alert(helloWorld("es"));
+ 
+  
