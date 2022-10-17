195d0f46d312bc5769464e6c7bdb2b6db877ed46
barisgit <baris.durucan@powercoders.org>
2022-10-17 20:38:16 +0200

    retirecalc

---
+++ retirecalc/retire.js
@@ -0,0 +1,8 @@
+
+let ageCurrent = prompt ("Please enter your age");
+let ageRetirement = prompt ("please enter the age you want to retire");
+let yearCurrent = 2020;
+let yearLeft = (ageRetirement - ageCurrent)
+let yearRetirement = yearCurrent + yearLeft;
+alert(' You are ' + ageCurrent + ' years old. ' + ' You will be retired at ' + ageRetirement + ' you have ' + yearLeft + ' years left until you can retire It is ' + yearCurrent + ' so you can retire in ' + yearRetirement);
+
