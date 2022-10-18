936abec0eb0883e74117cc2ca3173f93a2fffbec
barisgit <baris.durucan@powercoders.org>
2022-10-18 21:21:11 +0200

    some exercises templates

---
+++ mathsignesexercises/agecalc.js
@@ -0,0 +1,117 @@
+
+/*
+let birthYear = 1990;
+let futureYear = 2022;
+let birthMonth = 4;
+let futureMonth = 12;
+let ageBeforeBirthday = futureYear-birthYear-1;
+let ageAfterBirthday = futureYear-birthYear;
+
+
+if(futureMonth < birthMonth){
+    document.write('You will be ' + ageBeforeBirhtday + ' in ' + futureYear);
+} else {
+    document.write('You will be ' + ageAfterBirthday + ' in ' + futureYear)
+}  
+*/
+
+/*
+let age = 17
+let message = (age >=18) ? "major" : "minor"
+document.write(message); /*question mark makes the difference*/
+
+
+
+
+/*
+const ageBeforeBirthday = futureYear - birthYear;
+const ageAfterBirthday = ageBeforeBirthday - 1;
+document.write('I will be either ' + ageBeforeBirthday + ' or ' + ageAfterBirthday + ' in ' + futureYear);
+
+
+/*
+if(myValue>10){
+    console.log("My value is bigger then 10")
+}
+
+if(myValue>=10){
+    console.log("My value is equal to 10")
+}
+
+if(myValue>10 || myValue==10){
+    window.alert("Hello my friend. My value is equal to 10");
+}else{
+    window.alert("Hello my friend. My value is smaller then 10"); 
+}
+
+if(myFirstString == "Hello World"){
+    window.alert("Hello World");
+}else{
+    window.alert("not saying hello world");
+}
+*/
+/*
+let fruit ="apple"
+let message;
+
+switch(fruit){
+
+    case "apple":
+        message = "smile"
+        break;
+    
+    case "grape":
+        message = "cry"
+        break;
+    case "strawberry":
+        message = "laugh"
+        break;
+    case "banana":
+        message = "sleep"
+        break;
+}
+document.write(message);
+*/
+
+/*
+let numberOfChildren = 5;
+let partnerName = "Aicha";
+let geogLocation = "Lausanne";
+let jobTitle = "Cooker";
+
+document.write("You will be " + jobTitle + " in " + geogLocation + " and married to " + partnerName + " with " + numberOfChildren + " kids");
+*/
+
+/*
+var birthYear = 1984;
+var futureYear  = 2027;
+var age  = futureYear - birthYear;
+console.log('I will be either ' + age + ' or ' + (age - 1) + ' in ' + futureYear);
+
+*/
+
+let myValue = 15;
+let myFirstString = "Hello World";
+if(myValue==10){
+    console.log("My value is equal to 10")
+}
+
+if(myValue>10){
+    console.log("My value is bigger then 10")
+}
+
+if(myValue>=10){
+    console.log("My value is equal to 10")
+}
+
+if(myValue>10 || myValue==10){
+    window.alert("Hello my friend. My value is equal to 10");
+}else{
+    window.alert("Hello my friend. My value is smaller then 10"); 
+}
+
+if(myFirstString == "Hello World"){
+    window.alert("Hello World");
+}else{
+    window.alert("not saying hello world");
+}
