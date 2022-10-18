b9409ed0416939ef8938631d992b33cfd1d9bef4
barisgit <baris.durucan@powercoders.org>
2022-10-18 10:55:25 +0200

    array studies

---
+++ 5week/arraystudy/code.js
@@ -0,0 +1,35 @@
+
+var fruits=["Banana", "Apples", "Oranges", "Blueberries"];
+
+console.log(fruits[2]);
+fruits.shift();
+console.log(fruits);
+fruits.sort();
+console.log(fruits);
+fruits.push("Kiwi");
+console.log(fruits);
+fruits.shift();
+console.log(fruits);
+fruits.reverse();
+console.log(fruits);
+
+
+
+//console.log(fruits[2]);
+//console.log(fruits);
+//fruits.push("Kiwi");
+//console.log(fruits);
+//fruits.sort();(fruits)
+//console.log(fruits);
+//fruits.splice(1,2);
+//console.log(fruits);
+//fruits.push("Banana");
+//console.log(fruits);
+//fruits.sort();
+//console.log(fruits);
+//document.getElementById("fruits").innerHTML = fruits[1];
+
+//console.log(fruits.indexOf("Oranges"));
+//console.log(fruits[1]);
+//console.log(fruits.shift())
+//console.log(fruits)
